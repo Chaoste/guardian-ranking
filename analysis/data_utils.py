7f6e0a25385b2cf24e9a504bd0dfba21df06aab0
Thomas Kellermeier <t-kellermeier@hotmail.de>
2018-07-10 10:28:44 +0200

    publish results

--- analysis/data_utils.py
+++ analysis/data_utils.py
@@ -22,6 +22,28 @@ def get_comments_article_ids(comments):
 def read_chunkwise(comments_filepath, articles, quiet=False):
     article_ids = set(articles['article_id'])
     data_comments_pol_root = []  # Only parent comments
+    headline = pd.read_csv(comments_filepath, nrows=10)
+    for df_chunk in pd.read_csv(comments_filepath, header=None, skiprows=0, chunksize=1000000):
+        if not quiet:
+            print('.', end='')
+        matches = df_chunk[df_chunk[0].isin(article_ids)]
+        # Remove replies
+        matches = matches[matches[5].isnull()]  # index 5 is 'parent_comment_id'
+        if len(matches):
+            if not quiet:
+                print('({})'.format(len(matches)), end='')
+            data_comments_pol_root.append(matches)
+    data_comments_pol_root = pd.concat(data_comments_pol_root)
+    data_comments_pol_root.columns = headline.columns  # shape = (40974, 7)
+    # Remove parent_comment_id because it's NaN everywhere in this dataframe
+    columns = list(headline.columns)
+    columns.remove('parent_comment_id')
+    data_comments_pol_root = data_comments_pol_root[columns]  # shape = (40974, 6)
+    return data_comments_pol_root
+
+def read_all_chunkwise(comments_filepath, articles, quiet=False):
+    article_ids = set(articles['article_id'])
+    data_comments_pol_root = []  # Only parent comments
     data_comments_pol_all = []  # Includes replies
     headline = pd.read_csv(comments_filepath, nrows=10)
     for df_chunk in pd.read_csv(comments_filepath, header=None, skiprows=0, chunksize=1000000):
@@ -47,6 +69,7 @@ def read_chunkwise(comments_filepath, articles, quiet=False):
 
     data_comments_pol_all = pd.concat(data_comments_pol_all)
     data_comments_pol_all.columns = headline.columns  # shape = (?, 7)
+    return data_comments_pol_root, data_comments_pol_all
 
 def load_data(comments_filepath):
     data_articles = pd.read_csv(SRC_ARTICLES)  # shape = (626395, 2)
@@ -54,6 +77,18 @@ def load_data(comments_filepath):
     data_comments_pol = pd.read_csv(comments_filepath)
     return (data_articles, data_authors, data_comments_pol)
 
+# Add rel_upvotes, total_upvotes and rank to comments df
+def normalize(comments):
+    print('Add rank attribute')
+    comments['rank'] = comments.groupby('article_id')['timestamp'].rank(method='dense').astype(int)
+
+    print('Add article attributes')
+    comments['total_upvotes'] = comments.groupby('article_id')['upvotes'].transform('sum')
+    comments['total_comments'] = comments.groupby('article_id')['upvotes'].transform('count')
+
+    print('Apply normalization')
+    comments['rel_upvotes'] = comments.apply(lambda row: (row.upvotes / (row.total_upvotes or 1)) * 100, axis=1)
+    return co
 
 def extract_features(comments):
     print('Preparing features')
@@ -75,5 +110,15 @@ def extract_features(comments):
     extracted_features['author_upvotes'] = upvotes_per_author.loc[comments['author_id']].values
     extracted_features['author_comments'] = comments_per_author.loc[comments['author_id']].values
     extracted_features['mean_author_upvotes'] = mean_upvotes_per_author.loc[comments['author_id']].values
+
+    extracted_features['rank'] = comments.groupby('article_id')['timestamp'].rank(method='dense').astype(int)
+    extracted_features['total_upvotes'] = comments.groupby('article_id')['upvotes'].transform('sum')
+    extracted_features['total_comments'] = comments.groupby('article_id')['upvotes'].transform('count')
+    extracted_features['rel_upvotes'] = [
+        (upvotes / (total_upvotes or 1)) * 100
+        for upvotes, total_upvotes
+        in zip(extracted_features['upvotes'], extracted_features['total_upvotes'])
+    ]
+    # extracted_features['rel_upvotes'] = extracted_features.apply(lambda row: (row.upvotes / (row.total_upvotes or 1)) * 100, axis=1)
 
     return pd.DataFrame.from_dict(extracted_features)
--- analysis/viz_utils.py
+++ analysis/viz_utils.py
@@ -49,3 +49,16 @@ def plot_timestamps(article_id):
     datetimes = data[data['article_id'] == article_id]['timestamp']
     timestamps = [int(datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%SZ").timestamp()) for x in datetimes]
     plt.hist(timestamps)
+
+def plot_feature_correlations(extracted_features_df, author_comments=0):
+    labels = list(reversed(extracted_features_df.columns))
+
+    fig, ax = plt.subplots(1, 1, figsize=(14, 12))
+    cax = ax.matshow(extracted_features_df[extracted_features_df['author_comments'] > author_comments][labels].corr())
+    cbar = fig.colorbar(cax)
+    ax.set_xticklabels([''] + labels, rotation=45, ha='left')
+    ax.set_yticklabels([''] + labels)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.set_title(rf'Correlations: Comment Features ($comments_{{author}} \geq {author_comments}$)', y=1.15)
+    fig.savefig(f'correlations-{author_comments}.pdf')
